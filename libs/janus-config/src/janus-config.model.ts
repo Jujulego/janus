da9cd4f536b5a3b76fd69ff37ae017040ba4aeb5
Julien Capellari <julien.capellari@gmail.com>
2021-05-06 22:49:53 +0200

    add(workspace): janus-config library

---
+++ libs/janus-config/src/janus-config.model.ts
@@ -0,0 +1,32 @@
+// Interfaces
+export interface IGateConfig {
+  // Attributes
+  target: string;
+  enabled?: boolean;
+  changeOrigin?: boolean;
+  secure?: boolean;
+  ws?: boolean;
+}
+
+export interface IServiceConfig {
+  // Attributes
+  url: string;
+  gates: Record<string, IGateConfig>;
+}
+
+export interface IControlServerConfig {
+  // Attributes
+  port?: number;
+}
+
+export interface IProxyConfig {
+  // Attributes
+  port?: number;
+}
+
+export interface IJanusConfig {
+  // Attributes
+  proxy?: IProxyConfig;
+  control?: IControlServerConfig;
+  services: Record<string, IServiceConfig>;
+}

a037329336bce6181440b65231f49937f2b4de65
Julien Capellari <julien.capellari@gmail.com>
2021-05-15 16:19:02 +0200

    update(janus-common): add service model

--- libs/janus-common/src/index.ts
+++ libs/janus-common/src/index.ts
@@ -1,1 +1,2 @@
 export * from './gate';
+export * from './service';
---
+++ libs/janus-common/src/service.ts
@@ -0,0 +1,19 @@
+import gql from 'graphql-tag';
+
+import { IGate } from './gate';
+
+// Model
+export interface IService {
+  // Attributes
+  name: string;
+  url: string;
+  gates: IGate[];
+}
+
+// Fragment
+export const ServiceFragment = gql`
+    fragment Service on Service {
+        name
+        url
+    }
+`;

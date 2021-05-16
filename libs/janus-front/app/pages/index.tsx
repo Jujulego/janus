e160ee508ed34a28c1930b853dca8191ab574a59
Julien Capellari <julien.capellari@gmail.com>
2021-05-16 16:46:04 +0200

    update(janus-front): setup router

--- libs/janus-front/app/pages/index.tsx
+++ libs/janus-front/app/pages/index.tsx
@@ -1,9 +1,13 @@
-import { Divider, Grid, List, ListItem, ListItemText, ListSubheader } from '@material-ui/core';
+import { Divider, Grid, List, ListItem, ListItemText, ListSubheader, Typography } from '@material-ui/core';
+import { classToPlain } from 'class-transformer';
 import { GetServerSideProps, NextPage } from 'next';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 import { IService } from '@jujulego/janus-common';
 
 import { getDataService } from '../src/data';
+import { useMemo } from 'react';
 
 // Page
 export interface HomeProps {
@@ -13,29 +17,43 @@ export interface HomeProps {
 const Home: NextPage<HomeProps> = (props) => {
   const { services } = props;
 
+  // Contexts
+  const router = useRouter();
+
+  // Memos
+  const service = useMemo(() => {
+    return services.find((srv) => srv.name === router.query.service);
+  }, [services, router.query]);
+
   // Render
   return (
     <Grid container sx={{ flex: 1 }}>
       <Grid item xs={2}>
         <List
+          component="nav"
           subheader={
-            <ListSubheader>
+            <ListSubheader component="span">
               Services
             </ListSubheader>
           }
         >
           { services.map((service) => (
-            <ListItem button key={service.name}>
-              <ListItemText
-                primary={service.name}
-                secondary={service.url}
-              />
-            </ListItem>
+            <Link key={service.name} href={`/?service=${service.name}`} shallow passHref>
+              <ListItem button component="a">
+                <ListItemText
+                  primary={service.name}
+                  secondary={service.url}
+                />
+              </ListItem>
+            </Link>
           )) }
         </List>
       </Grid>
       <Divider orientation="vertical" flexItem />
-      <Grid item xs />
+      <Grid item xs>
+        <Typography>{ service?.name }</Typography>
+        <Typography>{ service?.url }</Typography>
+      </Grid>
     </Grid>
   );
 };
@@ -48,7 +66,7 @@ export const getServerSideProps: GetServerSideProps<HomeProps> = async (ctx) => 
 
   return {
     props: {
-      services: JSON.parse(JSON.stringify(data.listServices()))
+      services: classToPlain(data.listServices()) as IService[]
     }
   };
 };

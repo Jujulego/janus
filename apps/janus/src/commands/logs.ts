715a0c3b8d2e116ea6584dc33b5e8dfc4ae50bfc
Julien Capellari <julien.capellari@neoxia.com>
2021-06-07 15:52:58 +0200

    update(janus): logs follow option

--- apps/janus/src/commands/logs.ts
+++ apps/janus/src/commands/logs.ts
@@ -1,7 +1,9 @@
 import chalk from 'chalk';
 import { GraphQLClient } from 'graphql-request';
 import gql from 'graphql-tag';
+import { SubscriptionClient } from 'subscriptions-transport-ws';
 import { CommandBuilder } from 'yargs';
+import WebSocket from 'ws';
 
 import { ILog, LogFragment } from '@jujulego/janus-common';
 import { JanusConfig } from '@jujulego/janus-config';
@@ -14,20 +16,30 @@ interface ILogsData {
   logs: ILog[];
 }
 
+interface LogsArgs extends CommonArgs {
+  follow: boolean;
+}
+
 // Command
 export const command = 'logs';
 export const describe = 'Print proxy logs';
 
-export const builder: CommandBuilder = {};
+export const builder: CommandBuilder = {
+  follow: {
+    alias: 'f',
+    boolean: true,
+    default: false,
+  }
+};
 
-export const handler = commandWrapper(async (args: CommonArgs) => {
+export const handler = commandWrapper(async (args: LogsArgs) => {
   try {
     // Load config
     logger.spin('Connecting to proxy server ...');
     const config = await JanusConfig.loadFile(args.config, { logger });
 
     // Existing logs
-    const client = new GraphQLClient(`http://localhost:${config.control.port}/graphql`,);
+    const client = new GraphQLClient(`http://localhost:${config.control.port}/graphql`);
     const { logs } = await client.request<ILogsData>(gql`
       query Logs {
         logs {
@@ -52,6 +64,49 @@ export const handler = commandWrapper(async (args: CommonArgs) => {
       }
 
       logger.log(level as LogLevel, message);
+    }
+
+    // Subscribe to events
+    if (args.follow) {
+      const sclient = new SubscriptionClient(
+        `http://localhost:${config.control.port}/graphql`,
+        { lazy: true, reconnect: true },
+        WebSocket,
+      );
+
+      const obs = sclient.request({
+        query: gql`
+          subscription Logs {
+            logs {
+              ...Log
+            }
+          }
+
+          ${LogFragment}
+        `
+      });
+
+      obs.subscribe({
+        next(value) {
+          const log = value.data?.logs as ILog;
+          if (!log) return;
+
+          let { message, level } = log;
+
+          if (log.metadata.context) {
+            message = chalk`{grey [${log.metadata.context}]} ${log.message}`;
+          }
+
+          if (!LOG_LEVELS.includes(level)) {
+            level = 'info';
+          }
+
+          logger.log(level as LogLevel, message);
+        },
+        error(error) {
+          logger.error(error);
+        },
+      });
     }
   } catch (error) {
     console.error(error);

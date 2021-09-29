c2380111b3e42fece216cd20fc6c49efa755f00d
Julien Capellari <julien.capellari@gmail.com>
2021-09-29 19:49:08 +0200

    update(front): setup react hello world

---
+++ packages/front/src/index.tsx
@@ -0,0 +1,11 @@
+import ReactDOM from 'react-dom';
+
+// Polyfills
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
+// Start App
+ReactDOM.render(
+  <h1>Hello World!</h1>,
+  document.getElementById('root')
+);

8c11e3eac196ee9fa1cac1917669d4abe518b438
Julien Capellari <julien@capellari.net>
2021-10-03 23:36:42 +0200

    update(front): copy service logs logic

--- packages/front/src/pages/ServicePage.tsx
+++ packages/front/src/pages/ServicePage.tsx
@@ -1,10 +1,12 @@
 import { useGraphql } from '@jujulego/alma-graphql';
-import { IService, ServiceFragment } from '@jujulego/janus-types';
+import { ILog, IService, ServiceFragment } from '@jujulego/janus-types';
+import { Box } from '@mui/material';
 import { gql } from 'graphql.macro';
-import { FC } from 'react';
+import { FC, useCallback, useMemo, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 import { ServiceHeader } from '../molecules/ServiceHeader';
+import { Logs } from '../molecules/Logs';
 
 // Types
 export interface ServicePageData {
@@ -15,6 +17,9 @@ export interface ServicePageData {
 const ServicePage: FC = () => {
   // Router
   const { name } = useParams<{ name: string }>();
+
+  // State
+  const [selected, setSelected] = useState<string>(name);
 
   // Api
   const { data } = useGraphql<ServicePageData>('/graphql', gql`
@@ -27,13 +32,39 @@ const ServicePage: FC = () => {
       ${ServiceFragment}
   `, { name });
 
+  // Memos
+  const gate = useMemo(
+    () => data && data.service.gates.find((g) => g.name === selected),
+    [selected, data],
+  );
+
+  // Callbacks
+  const filterLogs = useCallback((log: ILog) => {
+    if (data) {
+      if (log.metadata.service !== data.service.name) {
+        return false;
+      }
+
+      if (gate) {
+        return log.metadata.gate === gate.name;
+      }
+    }
+
+    return true;
+  }, [data, gate]);
+
   // Render
   if (!data) {
     return null;
   }
 
   return (
-    <ServiceHeader service={data.service} />
+    <>
+      <ServiceHeader service={data.service} />
+      <Box mt={2} flex={1} minHeight={242}>
+        <Logs title={`Logs of ${selected === name ? selected : [name, selected].join('.')}`} filter={filterLogs} />
+      </Box>
+    </>
   );
 };
 

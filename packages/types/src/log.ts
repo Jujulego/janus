0162bbf289d11629b207a470c6606aba4c5a6fc5
Julien Capellari <julien@capellari.net>
2021-10-04 00:05:24 +0200

    update(types): use babel to reduce front's bundle

--- packages/types/src/log.ts
+++ packages/types/src/log.ts
@@ -1,4 +1,4 @@
-import gql from 'graphql-tag';
+import { gql } from 'graphql.macro';
 
 // Model
 export interface ILog {
--- packages/types/src/service.ts
+++ packages/types/src/service.ts
@@ -1,4 +1,4 @@
-import gql from 'graphql-tag';
+import { gql } from 'graphql.macro';
 
 import { GateFragment, IGate } from './gate';
 

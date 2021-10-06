087351d30888b9715568599b48777e4cd60b3dcc
Julien Capellari <julien@capellari.net>
2021-10-07 01:37:54 +0200

    update(types): build with gulp

--- packages/types/gulpfile.js
+++ packages/types/gulpfile.js
@@ -3,6 +3,11 @@ const babel = require('gulp-babel');
 const ts = require('gulp-typescript');
 
 // Config
+const paths = {
+  src: 'src/**/*.ts',
+  assets: 'src/config.schema.json',
+};
+
 const tsProject = ts.createProject('tsconfig.json', {
   isolatedModules: false,
   noEmit: false,
@@ -10,16 +15,19 @@ const tsProject = ts.createProject('tsconfig.json', {
 });
 
 // Tasks
-const cjs = () => gulp.src('src/**/*.ts')
+const cjs = () => gulp.src(paths.src)
   .pipe(babel({ envName: 'cjs' }))
   .pipe(gulp.dest('dist/cjs'));
 
-const esm = () => gulp.src('src/**/*.ts')
+const esm = () => gulp.src(paths.src)
   .pipe(babel({ envName: 'esm' }))
   .pipe(gulp.dest('dist/esm'));
 
-const typings = () => gulp.src('src/**/*.ts')
+const typings = () => gulp.src(paths.src)
   .pipe(tsProject()).dts
   .pipe(gulp.dest('dist/types'));
 
-gulp.task('build', gulp.parallel(cjs, esm, typings));
+const assets = () => gulp.src(paths.assets)
+  .pipe(gulp.dest('dist'));
+
+gulp.task('build', gulp.parallel(cjs, esm, typings, assets));

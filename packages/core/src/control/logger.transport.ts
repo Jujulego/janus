5292c86de56e6e03cb8eba2be2fbb6978154ef5e
Julien Capellari <julien@capellari.net>
2021-10-15 00:11:30 +0200

    feat(control): stream logs from file

--- packages/core/src/control/logger.transport.ts
+++ packages/core/src/control/logger.transport.ts
@@ -1,55 +1,23 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { plainToClass } from 'class-transformer';
 import { PubSub } from 'graphql-subscriptions';
-import Transport from 'winston-transport';
-import { format } from 'winston';
-
-import { Logger } from '../logger';
 
 import { Log } from './log.model';
+import { Logger } from '../logger';
 
 // Services
 @Injectable()
-export class LoggerTransport extends Transport implements OnModuleInit {
-  // Attributes
-  readonly history: Log[] = [];
-
+export class LoggerTransport implements OnModuleInit {
   // Constructor
-  constructor(private readonly _pubsub: PubSub) {
-    super({
-      format: format.combine(
-        format.metadata(),
-        format.timestamp()
-      )
-    });
-  }
+  constructor(private readonly _pubsub: PubSub) {}
 
   // Lifecycle
   onModuleInit(): void {
-    Logger.root.add(this);
-  }
-
-  // Methods
-  private _save(log: Log) {
-    this.history.push(log);
-  }
-
-  async log(info: any, next?: () => void): Promise<void> {
-    // Start next transport as soon as possible
-    if (next) {
-      setImmediate(next);
-    }
-
-    // Store and send via pubsub
-    try {
-      const log = plainToClass(Log, info);
-
-      this._save(log);
-      await this._pubsub.publish('logs', { logs: log });
-
-      this.emit('logged', info);
-    } catch (error) {
-      this.emit('warn', error);
-    }
+    Logger.root.stream({ start: -1 }).on('log', async ({ message, level, timestamp, transport, ...metadata }) => {
+      if (transport.includes('file')) {
+        const log = plainToClass(Log, { message, level, timestamp, metadata });
+        await this._pubsub.publish('logs', { logs: log });
+      }
+    });
   }
 }
